6894a3375fa98f935c2e1a974233f9694ad51d92
tomasz.swienty-symfonia <tomasz.swienty@symfonia.pl>
2025-01-13 14:25:58 +0100

    sadasd

--- backend/app/app/Http/Controllers/Api/AuthController.php
+++ backend/app/app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
