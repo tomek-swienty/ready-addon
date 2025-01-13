2114fe3dbca6b6f77187bee3313103ccb3bdb242
tomasz.swienty-symfonia <tomasz.swienty@symfonia.pl>
2025-01-13 15:32:55 +0100

    ddd

--- backend/app/routes/api.php
+++ backend/app/routes/api.php
@@ -1,13 +1,23 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TodoController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:api')->group(function() {
-    Route::get('/me', [UserController::class, 'me']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
+});
+
+Route::controller(TodoController::class)->group(function () {
+    Route::get('todos', 'index');
+    Route::post('todo', 'store');
+    Route::get('todo/{id}', 'show');
+    Route::put('todo/{id}', 'update');
+    Route::delete('todo/{id}', 'destroy');
 });
